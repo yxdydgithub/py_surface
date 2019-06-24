d0e79f16c623dc6e6612d82cf10a81498cc871c6
杨晓东 <yangxiaodong@yxd.local>
2019-06-25 01:16:53 +0800

    初次实现简单界面

--- sj/SJApp.py
+++ sj/SJApp.py
@@ -9,16 +9,22 @@ from kivy.uix.scatter import Scatter
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.relativelayout import RelativeLayout
 
 
-class ScatterTextWidget(BoxLayout):
+from kivy.uix.dropdown import DropDown
+from kivy.uix.button import Button
+from kivy.base import runTouchApp
+
+
+class MyWidget(RelativeLayout):
     pass
 
 class SJApp(App):
 
     def build(self):
-        return ScatterTextWidget()
+        return MyWidget()
 
-if __name__ == '__main__':
-        SJApp().run()
+if __name__ == "__main__":
+    SJApp().run()
+
